b543a95ea4be84296c5034461e9225f4d34d6a01
Hew Wolff <hewwolff@sonic.net>
2017-01-25 13:28:54 -0800

    get simple case running

--- test_calc.rb
+++ test_calc.rb
@@ -3,10 +3,12 @@ require 'test/unit'
  
 class TestCalc < Test::Unit::TestCase
   def setup
+    @calc = Calc.new
   end
   
-  def test
-    calc = Calc.new
-    assert_equal 2, calc.evaluate("1+1")
+  def test_simple
+    assert_equal 1, calc.evaluate("1")
+    assert_equal 3, calc.evaluate("1+2")
+    assert_equal 6, calc.evaluate("1+2+3")
   end
 end
