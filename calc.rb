97480a27923b4b6b38f9127039c2c9213cc3c16e
Hew Wolff <hewwolff@sonic.net>
2017-01-25 13:42:22 -0800

    add minus

--- calc.rb
+++ calc.rb
@@ -3,12 +3,19 @@ class Calc
   end
 
   def evaluate(expression)
-    add_terms = expression.split(/\+/)
-    add_ops = ['+'] + expression.scan(/\+/).to_a
+    #puts "expression: #{expression}"
+    ops_re = /[+-]/
+    add_terms = expression.split(ops_re)
+    #puts "add_terms: #{add_terms}"
+    add_ops = ['+'] + expression.scan(ops_re).to_a
+    #puts "add_ops: #{add_ops}"
     op_terms = add_ops.zip(add_terms)
     op_terms.reduce(0) do |acc, v|
+      #puts "acc: #{acc}"
       is_negative = v[0] == '-'
+      #puts "is_neg: #{is_negative}"
       next_value = evaluate_add_term(v[1])
+      #puts "value: #{next_value}"
       acc + (is_negative ? -next_value : next_value)
     end
   end
--- test_calc.rb
+++ test_calc.rb
@@ -11,4 +11,11 @@ class TestCalc < Test::Unit::TestCase
     assert_equal 3, @c.evaluate("1+2")
     assert_equal 6, @c.evaluate("1+2+3")
   end
+  
+  def test_negative
+    assert_equal -1, @c.evaluate("1-2")
+    assert_equal 2, @c.evaluate("1-2+3")
+    assert_equal -4, @c.evaluate("1-2-3")
+    assert_equal -2, @c.evaluate("1-2+3-4")
+  end
 end
