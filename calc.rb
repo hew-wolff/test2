0722d444b7b44bb7e2654f65004d3624d7db0879
Hew Wolff <hewwolff@sonic.net>
2017-01-25 13:48:26 -0800

    add multiplication

--- calc.rb
+++ calc.rb
@@ -20,7 +20,25 @@ class Calc
     end
   end
 
-  def evaluate_add_term(s)
-    s.to_f
+  def evaluate_add_term(expression)
+    ops_re = /[*]/
+    terms = expression.split(ops_re)
+    #puts "add_terms: #{add_terms}"
+    ops = ['*'] + expression.scan(ops_re).to_a
+    #puts "add_ops: #{add_ops}"
+    op_terms = ops.zip(terms)
+    op_terms.reduce(1) do |acc, v|
+      #puts "acc: #{acc}"
+      #is_negative = v[0] == '-'
+      #puts "is_neg: #{is_negative}"
+      next_value = evaluate_simple_term(v[1])
+      #puts "value: #{next_value}"
+      #acc + (is_negative ? -next_value : next_value)
+      acc * next_value
+    end
+  end
+
+  def evaluate_simple_term(t)
+    t.to_f
   end
 end
--- test_calc.rb
+++ test_calc.rb
@@ -6,16 +6,24 @@ class TestCalc < Test::Unit::TestCase
     @c = Calc.new
   end
   
-  def test_simple
+  def test_plus
     assert_equal 1, @c.evaluate("1")
     assert_equal 3, @c.evaluate("1+2")
     assert_equal 6, @c.evaluate("1+2+3")
   end
   
-  def test_negative
+  def test_minus
     assert_equal -1, @c.evaluate("1-2")
     assert_equal 2, @c.evaluate("1-2+3")
     assert_equal -4, @c.evaluate("1-2-3")
     assert_equal -2, @c.evaluate("1-2+3-4")
+  end
+  
+  def test_mult
+    assert_equal 2, @c.evaluate("1*2")
+    assert_equal 5, @c.evaluate("1*2+3")
+    assert_equal 6, @c.evaluate("1*2*3")
+    assert_equal 7, @c.evaluate("1+2*3")
+    assert_equal 30, @c.evaluate("1+2*3*4+5")
   end
 end
