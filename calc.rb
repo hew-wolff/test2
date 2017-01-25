18b3a079c4e92d68d504246f1e87dc6c8abc0adc
Hew Wolff <hewwolff@sonic.net>
2017-01-25 13:35:02 -0800

    expand a little

--- calc.rb
+++ calc.rb
@@ -4,8 +4,9 @@ class Calc
 
   def evaluate(expression)
     add_terms = expression.split(/\+/)
-    add_ops = ['+'] + expression.match(/\+/).to_a
-    add_ops.zip(add_terms).reduce(0) do |acc, v|
+    add_ops = ['+'] + expression.scan(/\+/).to_a
+    op_terms = add_ops.zip(add_terms)
+    op_terms.reduce(0) do |acc, v|
       is_negative = v[0] == '-'
       next_value = evaluate_add_term(v[1])
       acc + (is_negative ? -next_value : next_value)
--- test_calc.rb
+++ test_calc.rb
@@ -3,12 +3,12 @@ require 'test/unit'
  
 class TestCalc < Test::Unit::TestCase
   def setup
-    @calc = Calc.new
+    @c = Calc.new
   end
   
   def test_simple
-    assert_equal 1, calc.evaluate("1")
-    assert_equal 3, calc.evaluate("1+2")
-    assert_equal 6, calc.evaluate("1+2+3")
+    assert_equal 1, @c.evaluate("1")
+    assert_equal 3, @c.evaluate("1+2")
+    assert_equal 6, @c.evaluate("1+2+3")
   end
 end
